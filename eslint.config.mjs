444b76bebac7e8ef98c6caa85cfe91c8d0cb0cb9
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2025-03-19 21:21:26 -0600

    feat: config prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,4 +10,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    ignores: ["node_modules", "dist", "build", ".next"],
+  },
 ];
