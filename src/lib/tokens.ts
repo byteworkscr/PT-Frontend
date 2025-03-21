4122f294c12452da16235e82f912ddd1498deb71
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2025-03-20 18:56:14 -0600

    feat: optimized code

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -1,21 +1,12 @@
 export const tokens = [
   {
-    id: "14",
-    name: "Tether",
-    symbol: "USDT",
-    balance: 2000,
-    value: 2000,
-    iconSymbol: "usdt",
-    isStablecoin: true,
-  },
-  {
     id: "15",
     name: "USD Coin",
     symbol: "USDC",
     balance: 1500,
     value: 1500,
     iconSymbol: "usdc",
-    isStablecoin: true,
+    icon: "https://res.cloudinary.com/dfxes8tvx/image/upload/v1742518288/USDC_k4ow3i.webp",
   },
   {
     id: "4",
@@ -24,14 +15,16 @@ export const tokens = [
     balance: 2.5,
     value: 7500,
     iconSymbol: "eth",
+    icon: "https://res.cloudinary.com/dfxes8tvx/image/upload/v1742518327/ETH_maj8bk.svg",
   },
   {
-    id: "5",
-    name: "Solana",
-    symbol: "SOL",
-    balance: 50,
-    value: 5000,
-    iconSymbol: "sol",
+    id: "16",
+    name: "Stellar",
+    symbol: "XLM",
+    balance: 1000,
+    value: 1000,
+    iconSymbol: "xlm",
+    icon: "https://res.cloudinary.com/dfxes8tvx/image/upload/v1742516910/Stellar_xycxeb.webp",
   },
 ];
 
